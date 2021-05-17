d802f5c52011a8eb9f0ada6f3f5ab03ef3094af4
Vicky <vignesh13919999@gmail.com>
2021-05-17 10:37:35 +0530

    FilesUpadte

---
+++ ServiceDemo/app/app.module.ts
@@ -0,0 +1,53 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpModule } from '@angular/http';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { UserComponent } from './user/user.component';
+import { EmployComponent } from './employ/employ.component';
+import { CustomerComponent } from './customer/customer.component';
+import { EmploySearchComponent } from './employ-search/employ-search.component';
+import { FormsModule } from '@angular/forms';
+import { CustomerSearchComponent } from './customer-search/customer-search.component';
+import { UserSearchComponent } from './user-search/user-search.component';
+import { MenuComponent } from './menu/menu.component';
+import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+
+const appRoutes : Routes = [
+  {path:'',component:LoginComponent},
+  {path:'menu',component:MenuComponent,
+  children : [
+    {path:'user',component:UserComponent,outlet:'data'},
+  {path:'employ',component:EmployComponent,outlet:'data'},
+  {path:'Customer',component:CustomerComponent,outlet:'data'},
+  {path:'employsearch',component:EmploySearchComponent,outlet:'data'},
+  {path:'customersearch',component:CustomerSearchComponent,outlet:'data'},
+  {path:'usersearch',component:UserSearchComponent,outlet:'data'},
+  
+  ]
+}
+]
+@NgModule({
+  declarations: [
+    AppComponent,
+    UserComponent,
+    EmployComponent,
+    CustomerComponent,
+    EmploySearchComponent,
+    CustomerSearchComponent,
+    UserSearchComponent,
+    MenuComponent,
+    LoginComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpModule,
+    FormsModule,
+    RouterModule.forRoot(appRoutes),
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ ServiceDemo/app/user/user.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { User } from '../user';
+import { UserService } from '../user.service';
+@Component({
+  selector: 'app-user',
+  templateUrl: './user.component.html',
+  styleUrls: ['./user.component.css']
+})
+export class UserComponent implements OnInit {
+  users : Observable<User[]>;
+  constructor(private _userservice : UserService) {
+    this.users=this._userservice.showUsers();
+   }
+
+  ngOnInit(): void {
+  }
+
+}
